3a6e68872fc6f2431ba6cc53d58fdc5692eccca5
HideBa <baba.papa1120.ba@gmail.com>
2023-11-21 10:33:00 +0100

    wip: impl voronoi

--- ass1/my_code_hw01.py
+++ ass1/my_code_hw01.py
@@ -1,7 +1,7 @@
 # -- my_code_hw01.py
 # -- geo1015.2023.hw01
-# -- [YOUR NAME]
-# -- [YOUR STUDENT NUMBER]
+# -- Hidemichi Baba
+# -- 5967538
 
 import startinpy
 import numpy as np
@@ -60,8 +60,6 @@ class Tin:
 
     def get_voronoi_edges(self):
         """
-        !!! TO BE COMPLETED !!!
-
         Function that returns all the Voronoi edges of the bounded
         Voronoi cells in the dataset.
 
@@ -75,8 +73,24 @@ class Tin:
                    (function get_delaunay_edges() uses the same principle)
         """
         edges = []
+        triangles = self.dt.triangles
+        for i in range(0, len(triangles)):
+            tri1 = triangles[i]
+            tri2 = triangles[i + 1] if i + 1 < len(triangles) else triangles[0]
+            a1 = self.dt.points[tri1[0]]
+            b1 = self.dt.points[tri1[1]]
+            c1 = self.dt.points[tri1[2]]
+            a2 = self.dt.points[tri2[0]]
+            b2 = self.dt.points[tri2[1]]
+            c2 = self.dt.points[tri2[2]]
+            cc1 = self.circumcircle_center(a1, b1, c1)
+            cc2 = self.circumcircle_center(a2, b2, c2)
+            if cc1 is None or cc2 is None:
+                continue
+            edges.append([cc1, cc2])
         return edges
 
+    # Ref: https://github.com/hugoledoux/startin/blob/574e3c8cd06aa5b03b86f867b1fb69f21c0f81c5/src/interpolation/mod.rs#L174C1-L174C5
     def interpolate_tin(self, x, y):
         """
         !!! TO BE COMPLETED !!!
@@ -91,7 +105,25 @@ class Tin:
                numpy.nan if outside the convex hull
                (NaN: Not a Number https://numpy.org/devdocs/reference/constants.html#numpy.nan)
         """
-        return 123.45
+        q = (x, y)
+        # if not is_inside_convex_hull(q, convex_hull(self.dt.points)):
+        #     raise ValueError("Point is outside the convex hull")
+        # TODO: check if it allowed or not
+        if not self.dt.is_inside_convex_hull(q):
+            raise ValueError("Point is outside the convex hull")
+
+        # closest 3 points
+        sorted_points = self.closer_point((x, y))
+        p0, p1, p2 = sorted_points[0], sorted_points[1], sorted_points[2]
+        xyp1, xyp2, xyp3 = (p0[0], p0[1]), (p1[0], p1[1]), (p2[0], p2[1])
+        a0 = area_triangle(q, xyp2, xyp3)
+        a1 = area_triangle(q, xyp1, xyp3)
+        a2 = area_triangle(q, xyp1, xyp2)
+        total = 0.0
+        total += p0[2] * a0
+        total += p1[2] * a1
+        total += p2[2] * a2
+        return total / (a0 + a1 + a2)
 
     def get_area_voronoi_cell(self, vi):
         """
@@ -106,4 +138,88 @@ class Tin:
                return numpy.inf if the cell is unbounded
                (infinity https://numpy.org/devdocs/reference/constants.html#numpy.inf)
         """
+
         return np.inf
+
+    # TODO: change to implement this ref: https://github.com/hugoledoux/startin/blob/574e3c8cd06aa5b03b86f867b1fb69f21c0f81c5/src/geom/mod.rs#L18C3-L18C3
+    def circumcircle_center(self, p1, p2, p3):
+        ax, ay = p1[0], p1[1]
+        bx, by = p2[0], p2[1]
+        cx, cy = p3[0], p3[1]
+        d = 2.0 * (ax * (by - cy) + bx * (cy - ay) + cx * (ay - by))
+        if d == 0:
+            return None
+        ux = (
+            (ax**2 + ay**2) * (by - cy)
+            + (bx**2 + by**2) * (cy - ay)
+            + (cx**2 + cy**2) * (ay - by)
+        ) / d
+        uy = (
+            (ax**2 + ay**2) * (cx - bx)
+            + (bx**2 + by**2) * (ax - cx)
+            + (cx**2 + cy**2) * (bx - ax)
+        ) / d
+        # radius = math.dist((ux, uy), (ax, ay))
+        return (ux, uy)
+
+    def closer_point(self, point):
+        """
+        The function returns sorted points by distance from the given point.
+        Input:
+            point:     the position of vertex
+        Output:
+            sorted_points: sorted points by distance from the given point
+        """
+        points = self.dt.points
+        sorted_points = sorted(
+            points, key=lambda p: math.dist(p, point)
+        )  # Memo: Python's sorted method doesn't change original list
+        return sorted_points
+
+
+# Ref: https://github.com/hugoledoux/startin/blob/574e3c8cd06aa5b03b86f867b1fb69f21c0f81c5/src/geom/mod.rs#L14
+def area_triangle(a, b, c):
+    return det3x3t(a, b, c) / 2.0
+
+
+# Ref: https://github.com/hugoledoux/startin/blob/574e3c8cd06aa5b03b86f867b1fb69f21c0f81c5/src/geom/mod.rs#L10
+def det3x3t(a, b, c):
+    return ((a[0] - c[0]) * (b[1] - c[1])) - ((a[1] - c[1]) * (b[0] - c[0]))
+
+
+def orientation(p, q, r):
+    """Calculate the orientation of the triplet (p, q, r).
+    Returns positive if counter-clockwise, negative if clockwise, zero if collinear."""
+    return (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
+
+
+def convex_hull(points):
+    if len(points) <= 3:
+        # TODO: check later
+        return points
+    start = min(points, key=lambda p: (p[1], p[0]))
+
+    # Sort points by polar angle with start
+    def polar_angle(p):
+        return (p[0] - start[0]) ** 2 + (p[1] - start[1]) ** 2, -orientation(
+            start, p, (start[0], start[1] + 1)
+        )
+
+    sorted_points = sorted(points, key=polar_angle)
+
+    # Construct the hull
+    hull = [start]
+    for p in sorted_points[1:]:
+        while len(hull) > 1 and orientation(hull[-2], hull[-1], p) <= 0:
+            hull.pop()
+        hull.append(p)
+
+    return hull
+
+
+def is_inside_convex_hull(point, hull):
+    """Check if a point is inside the convex hull"""
+    for i in range(len(hull)):
+        if orientation(hull[i], hull[(i + 1) % len(hull)], point) < 0:
+            return False
+    return True
