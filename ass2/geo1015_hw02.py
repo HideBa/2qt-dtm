8d4c9b71f3962073984a5426be81bb25ba4c2337
HideBa <baba.papa1120.ba@gmail.com>
2023-12-02 15:34:12 +0100

    impl aspect and hillshade

--- ass2/geo1015_hw02.py
+++ ass2/geo1015_hw02.py
@@ -42,49 +42,79 @@ def main():
     debug_d = np.squeeze(d.read(1))
     np.savetxt("./ass2/debug/terrain.csv", debug_d, delimiter=",")
 
-    aspect_data = aspect(d)
-    with rasterio.open(
+    aspect_data, gradient_data = slope(d)
+    hillshade_data = hillshade(gradient_data, aspect_data)
+
+    save_ras(
+        aspect_data,
+        d,
         "./ass2/out/aspect.tiff",
-        "w",
-        driver="GTiff",
-        height=aspect_data.shape[0],
-        width=aspect_data.shape[1],
-        count=1,
-        dtype=np.uint16,
-        crs=d.crs,
-        transform=d.transform,
-    ) as dst:
-        dst.write(aspect_data, 1)
+    )
+    save_ras(hillshade_data, d, "./ass2/out/hillshade.tiff")
     read_aspect()
     np.savetxt("./ass2/debug/aspect-my.csv", aspect_data, delimiter=",")
 
 
-def aspect(d):
-    # return maximum_height_difference(d)
-    aspect, _ = finite_difference(d)
-    return aspect
+def hillshade(
+    gradient_degree, aspect_degree, sun_azimuth_degree=315, sun_height_degree=30
+):
+    # it expects that all parameters in degree
+    gradient = np.radians(gradient_degree)
+    aspect = np.radians(aspect_degree)
+    hillshade = np.zeros_like(gradient, dtype=np.float64)
+    print("sun", sun_azimuth_degree, sun_azimuth_degree)
+
+    if gradient.shape != aspect.shape:
+        print("different size!!!")
+        raise Exception("Gradient and Aspect should be same shape")
+
+    if sun_azimuth_degree < 0 or sun_azimuth_degree > 360:
+        raise Exception("invalid sun azimuth degree")
+
+    if sun_height_degree < 0 or sun_height_degree > 180:
+        raise Exception("invalid sun height")
+
+    sun_azimuth = math.radians(sun_azimuth_degree)
+    sun_height = math.radians(sun_height_degree)
+    n_rows, n_cols = gradient.shape
+    for i in range(n_rows):
+        for j in range(n_cols):
+            hillshade_ij = 255 * (
+                (math.cos(math.pi / 2 - sun_height) * math.cos(gradient[i][j]))
+                + (
+                    math.sin(math.pi / 2 - sun_height)
+                    * math.sin(gradient[i][j])
+                    * math.cos(sun_azimuth - aspect[i][j])
+                )
+            )
+            hillshade[i][j] = hillshade_ij
+    return hillshade
 
 
-def finite_difference(d):
-    n1 = d.read(1)
-    aspect_list = np.zeros_like(n1, dtype=np.float64)
-    gradient_list = np.zeros_like(n1, dtype=np.float64)
-    n_rows, n_cols = n1.shape
+def slope(d):
+    aspect, gradient = finite_difference(d.read(1))
+    return aspect, gradient
+
+
+def finite_difference(dtm):
+    aspect_list = np.zeros_like(dtm, dtype=np.float32)
+    gradient_list = np.zeros_like(dtm, dtype=np.float32)
+    n_rows, n_cols = dtm.shape
     for i in range(n_rows):
         for j in range(n_cols):
             if i == 0 or i == n_rows - 1 or j == 0 or j == n_cols - 1:
                 aspect_list[i][j] = 0
                 continue
 
-            dx = (n1[i - 1][j] - n1[i + 1][j]) / (2 * CELL_SIZE)
-            dy = (n1[i][j - 1] - n1[i][j + 1]) / (2 * CELL_SIZE)
-            gradient = math.atan(math.sqrt(dx**2 + dx**2))
-            # TODO: check when delta_z_x is 0
-            aspect = math.atan2(dy, dx)
+            dx = (dtm[i, j + 1] - dtm[i, j - 1]) / (2 * CELL_SIZE)
+            dy = (dtm[i + 1, j] - dtm[i - 1, j]) / (2 * CELL_SIZE)
+
+            gradient = math.degrees(math.atan(math.sqrt(dx**2 + dy**2)))
+            # TODO: consider when dx is zero
+            aspect = math.degrees(math.atan2(dy, dx))
             gradient_list[i][j] = gradient
-            aspect_list[i][j] = aspect
-    aspect_in_degree = np.degrees(aspect_list) % 360
-    return aspect_in_degree, gradient_list
+            aspect_list[i][j] = (aspect + 360) % 360 if aspect < 0 else aspect
+    return aspect_list, gradient_list
 
 
 def maximum_height_difference(d):
@@ -142,57 +172,24 @@ def gradient(d):
     n2 = np.zeros_like(n1, dtype=np.int8)
 
 
-# def neighbour_gradient(neighbours):
-#     center_index = len(neighbours)/2
-#     for n in neighbours:
-#         if index()
-
-
-def hillshade(d):
-    pass
+def save_ras(data, source_ras, out_path):
+    with rasterio.open(
+        out_path,
+        "w",
+        driver="GTiff",
+        height=data.shape[0],
+        width=data.shape[1],
+        count=1,
+        dtype=source_ras.dtypes[0],
+        crs=source_ras.crs,
+        transform=source_ras.transform,
+    ) as dst:
+        dst.write(data, 1)
 
 
 """
 The code below is sample or for debug
 """
-
-
-def some_code_to_help_with_rasterio(dataset, middlept):
-    """
-    !!! USE THIS CODE !!!
-
-    Some random operations with rasterio are shown below, they don't have
-    much meanings.
-    They are useful to learn how to read/write GeoTIFF files.
-    Use this code for your own function, copy part of it, it's allowed.
-    """
-    # -- numpy of input
-    n1 = dataset.read(1)
-    # -- an empty array with all values=0
-    # n2 = np.zeros(dataset.shape, dtype=np.int8) #-- you can define the type to use for each cell
-    n2 = np.zeros(dataset.shape)
-    for i in range(n1.shape[0]):
-        for j in range(n1.shape[1]):
-            n2[i][j] = 2 * n1[i][j]
-    # -- put middlept value=99
-    # -- index of p in the numpy raster
-    row, col = dataset.index(middlept[0], middlept[1])
-    n2[row, col] = 999
-    # -- write this to disk
-    output_file = "output.tiff"
-    with rasterio.open(
-        output_file,
-        "w",
-        driver="GTiff",
-        height=n2.shape[0],
-        width=n2.shape[1],
-        count=1,
-        dtype=n2.dtype,
-        crs=dataset.crs,
-        transform=dataset.transform,
-    ) as dst:
-        dst.write(n2, 1)
-    print("File written to '%s'" % output_file)
 
 
 def read_aspect():
